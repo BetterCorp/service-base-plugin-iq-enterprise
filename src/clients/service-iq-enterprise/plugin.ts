5f5f61edc6cc6a837d211ca6f10e5d3bcb8732a9
mrinc <github@mrincworld.com>
2023-07-20 19:41:36 +0200

    fixes

--- src/clients/service-iq-enterprise/plugin.ts
+++ src/clients/service-iq-enterprise/plugin.ts
@@ -1,5 +1,13 @@
-import { ServiceCallable, ServicesBase, ServicesClient } from "@bettercorp/service-base";
-import { APICustomer, IEmitAndReturn } from '../../index';
+import {
+  ServiceCallable,
+  ServicesBase,
+  ServicesClient,
+} from "@bettercorp/service-base";
+import {
+  APICustomerAccount,
+  APICustomerSubAccount,
+  IEmitAndReturn,
+} from "../../index";
 
 export class IQEnterpriseClient extends ServicesClient<
   ServiceCallable,
@@ -8,20 +16,62 @@ export class IQEnterpriseClient extends ServicesClient<
   ServiceCallable,
   ServiceCallable
 > {
-  public static readonly pluginName = 'service-iq-enterprise';
+  public static readonly pluginName = "service-iq-enterprise";
   public constructor(self: ServicesBase) {
     super(self);
   }
 
-  public async getCustomersByEmail(email: string): Promise<Array<APICustomer>>
-  public async getCustomersByEmail(email: string, hostname: string, username: string, password: string): Promise<Array<APICustomer>>
-  public async getCustomersByEmail(email: string, hostname?: string, username?: string, password?: string): Promise<Array<APICustomer>> {
-    return await this._plugin.emitEventAndReturn('getCustomersByEmail', email, hostname, username, password);
+  public async getCustomersByEmail(
+    email: string
+  ): Promise<Array<APICustomerAccount>>;
+  public async getCustomersByEmail(
+    email: string,
+    hostname: string,
+    username: string,
+    password: string
+  ): Promise<Array<APICustomerAccount>>;
+  public async getCustomersByEmail(
+    email: string,
+    hostname?: string,
+    username?: string,
+    password?: string
+  ): Promise<Array<APICustomerAccount>> {
+    return await this._plugin.emitEventAndReturn(
+      "getCustomersByEmail",
+      email,
+      hostname,
+      username,
+      password
+    );
   }
 
-  public async getCustomerById(id: number): Promise<APICustomer>
+  /*public async getCustomerById(id: number): Promise<APICustomer>
   public async getCustomerById(id: number, hostname: string, username: string, password: string): Promise<APICustomer>
   public async getCustomerById(id: number, hostname?: string, username?: string, password?: string): Promise<APICustomer> {
     return await this._plugin.emitEventAndReturn('getCustomerById', id, hostname, username, password);
+  }*/
+
+  public async getCustomerAccountById(
+    id: number
+  ): Promise<APICustomerSubAccount>;
+  public async getCustomerAccountById(
+    id: number,
+    hostname: string,
+    username: string,
+    password: string
+  ): Promise<APICustomerSubAccount>;
+  public async getCustomerAccountById(
+    id: number,
+    hostname?: string,
+    username?: string,
+    password?: string
+  ): Promise<APICustomerSubAccount> {
+    return await this._plugin.emitEventAndReturn(
+      "getCustomerAccountById",
+      id,
+      hostname,
+      username,
+      password
+    );
   }
 }
